3016fffffe2ec10d51efb6fd332db58d24435810
Андрій Кулик <andrijkulik@MacBook-Pro-Andrij.local>
2020-10-13 18:33:12 +0300

    tablet leyout component Tranaction done

--- frontend/src/Components/Header/Transaction/Transaction.js
+++ frontend/src/Components/Header/Transaction/Transaction.js
@@ -4,6 +4,14 @@ import styles from "./Transaction.module.css";
 const Transaction = () => {
   return (
     <div className={styles.temporary_wrapper}>
+      <div className={styles.transaction__header}>
+        <span className={styles.transaction__header_item}>Дата</span>
+        <span className={styles.transaction__header_item}>Тип</span>
+        <span className={styles.transaction__header_item}>Категория</span>
+        <span className={styles.transaction__header_item}>Комментарий</span>
+        <span className={styles.transaction__header_item}>Сумма</span>
+        <span className={styles.transaction__header_item}>Баланс</span>
+      </div>
       <ul className={styles.transaction__list}>
         <li className={styles.tarnsaction__list_item}>
           <span className={styles.tarnsaction__list_item_key}>Дата</span>
@@ -21,6 +29,34 @@ const Transaction = () => {
           <span className={styles.tarnsaction__list_item_key}>Комментарий</span>
           <span className={styles.tarnsaction__list_item_value}>
             Подарок жене
+          </span>
+        </li>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Сумма</span>
+          <span className={styles.tarnsaction__list_item_value}>300.00</span>
+        </li>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Баланс</span>
+          <span className={styles.tarnsaction__list_item_value}>6 900.00</span>
+        </li>
+      </ul>
+      <ul className={styles.transaction__list}>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Дата</span>
+          <span className={styles.tarnsaction__list_item_value}>04.01.19</span>
+        </li>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Тип</span>
+          <span className={styles.tarnsaction__list_item_value}>-</span>
+        </li>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Категория</span>
+          <span className={styles.tarnsaction__list_item_value}>Разное</span>
+        </li>
+        <li className={styles.tarnsaction__list_item}>
+          <span className={styles.tarnsaction__list_item_key}>Комментарий</span>
+          <span className={styles.tarnsaction__list_item_value}>
+            Подарок жене на день рождения
           </span>
         </li>
         <li className={styles.tarnsaction__list_item}>
