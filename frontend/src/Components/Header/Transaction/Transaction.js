9f810ef2d321a21f7de098a6292cbd0f3762b836
Андрій Кулик <andrijkulik@MacBook-Pro-Andrij.local>
2020-10-13 22:42:32 +0300

    before pull dev

--- frontend/src/Components/Header/Transaction/Transaction.js
+++ frontend/src/Components/Header/Transaction/Transaction.js
@@ -3,7 +3,7 @@ import styles from "./Transaction.module.css";
 
 const Transaction = () => {
   return (
-    <div className={styles.temporary_wrapper}>
+    <>
       <div className={styles.transaction__header}>
         <span className={styles.transaction__header_item}>Дата</span>
         <span className={styles.transaction__header_item}>Тип</span>
@@ -40,35 +40,7 @@ const Transaction = () => {
           <span className={styles.tarnsaction__list_item_value}>6 900.00</span>
         </li>
       </ul>
-      <ul className={styles.transaction__list}>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Дата</span>
-          <span className={styles.tarnsaction__list_item_value}>04.01.19</span>
-        </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Тип</span>
-          <span className={styles.tarnsaction__list_item_value}>-</span>
-        </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Категория</span>
-          <span className={styles.tarnsaction__list_item_value}>Разное</span>
-        </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Комментарий</span>
-          <span className={styles.tarnsaction__list_item_value}>
-            Подарок жене на день рождения
-          </span>
-        </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Сумма</span>
-          <span className={styles.tarnsaction__list_item_value}>300.00</span>
-        </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Баланс</span>
-          <span className={styles.tarnsaction__list_item_value}>6 900.00</span>
-        </li>
-      </ul>
-    </div>
+    </>
   );
 };
 
