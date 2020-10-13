4ad1431e3392437fd4b43413be54e46263b9e7e2
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-10-13 23:06:40 +0300

    f3

--- frontend/src/Containers/Login/Login.js
+++ frontend/src/Containers/Login/Login.js
@@ -2,63 +2,62 @@ import React from "react";
 import css from "./Login.module.css";
 
 const Login = () => {
-  return <div className={css.login}>
+return <div className={css.login}>
+        <div className={css.login__wrapper}>
+          <form onSubmit
+            className={css.login__wrapper_form}>
+            <p className={css.login__wrapper_form_logo}>
+               Wallet
+            </p>
 
-  <div
-  className={css.login__wrapper}>
-  <form onSubmit
-  className={css.login__wrapper_form}>
-    <p className={css.login__wrapper_form_logo}>
-      Wallet
-    </p>
-    {/* --------- email input --------- */}
-    <label
-    className={css.login__wrapper_form_email_icon}>
-    <input
-      className={css.login__wrapper_form_email}
-      id="email"
-      type="email"
-      placeholder="E-mail"
-      name="email"
-      onChange
-      required
-      autoFocus
-    />
-    </label>
+{/* --------- email input --------- */}
 
-    {/* --------- password input --------- */}
-    <label
-    className={css.login__wrapper_form_password_icon}>
-    <input
-      className={css.login__wrapper_form_password}
-      placeholder="Пароль"
-      id="password"
-      type="password"
-      name="password"
-      onChange
-      minLength="6"
-      maxLength="15"
-      required
-    />
-    </label>
-    {/* --------- buttons login/register --------- */}
-    <div>
-      <button type="submit"
-      className={css.login__wrapper_form_input}>
-        Вход
-      </button>
+            <label className={css.login__wrapper_form_email_icon}>
+              <input
+                className={css.login__wrapper_form_email}
+                id="email"
+                type="email"
+                placeholder="E-mail"
+                name="email"
+                onChange
+                required
+                autoFocus
+              />
+            </label>
 
-      <button
-        type="button"  className={css.login__wrapper_form_register}
-      >
-        Регистрация
-      </button>
-    </div>
+{/* --------- password input --------- */}
 
-    {/* --------- /form end --------- */}
-  </form>
-</div>;
-</div>
+            <label className={css.login__wrapper_form_password_icon}>
+              <input
+                className={css.login__wrapper_form_password}
+                placeholder="Пароль"
+                id="password"
+                type="password"
+                name="password"
+                onChange
+                minLength="6"
+                maxLength="15"
+                required
+              />
+            </label>
+
+{/* --------- buttons login/register --------- */}
+
+            <div>
+              <button type="submit"
+                className={css.login__wrapper_form_input}>
+                Вход
+              </button>
+
+              <button
+                type="button"  className={css.login__wrapper_form_register}
+              >
+                Регистрация
+              </button>
+            </div>
+         </form>
+        </div>;
+       </div>
 };
 
 export default Login;
