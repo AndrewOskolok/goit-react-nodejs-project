e9cfd227d11cd6ce582a3382ecd191a7affb698b
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-10-13 13:39:15 +0300

    st2

--- frontend/src/Containers/Login/Login.js
+++ frontend/src/Containers/Login/Login.js
@@ -2,7 +2,53 @@ import React from "react";
 import css from "./Login.module.css";
 
 const Authorization = () => {
-  return <div className={css.login}>Login page</div>;
+  return  <div
+  className={css.loginForm}>
+  <form onSubmit>
+    {/* --------- email input --------- */}
+    <input
+      className={css.loginInputEmail}
+      id="email"
+      type="email"
+      placeholder="E-mail"
+      name="email"
+      onChange
+      required
+      autoFocus
+    />
+
+    {/* --------- password input --------- */}
+
+    <input
+      className={css.loginInputPassword}
+      placeholder="Пароль"
+      id="password"
+      type="password"
+      name="password"
+      onChange
+      minLength="6"
+      maxLength="15"
+      required
+    />
+
+    {/* --------- buttons login/register --------- */}
+    <div>
+      <button type="submit"
+      className={css.loginInput}>
+        Вход
+      </button>
+
+
+      <button
+        type="button"  className={css.loginRegister}
+      >
+        Регистрация
+      </button>
+    </div>
+
+    {/* --------- /form end --------- */}
+  </form>
+</div>;
 };
 
 export default Authorization;
