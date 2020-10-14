d7ddbde9da3124b8bedc487dda3f3de7853c381a
amie-affection <amie.affection@gmail.com>
2020-10-15 01:48:31 +0300

    modified main

---
+++ frontend/src/Containers/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classNames from "classnames";
+import Header from "../../Components/Header/Header";
+import css from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <div className={css.main}>
+      <Header />
+      <div className={css.main__wrapper}>
+
+      </div>
+    </div>
+  );
+};
+
+export default Main;
