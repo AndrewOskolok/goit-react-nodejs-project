b9148dd65e241ba5cf1ae09a3a7c22e9aa14af80
amie-affection <amie.affection@gmail.com>
2020-10-14 13:56:07 +0300

    done header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className={css.header}>
       <div className={css.header__wrapper}>
-        <h1 className={css.header__title}>Wallet</h1>
+        <a href="/">
+          <h1 className={css.header__title}>Wallet</h1>
+        </a>
         <div className={css.header__login_wrapper}>
           <p className={css.header__login_name}>Имя</p>
           <button className={css.header__logout_button}></button>
