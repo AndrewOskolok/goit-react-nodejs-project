984127b1bd4b6407104ff98f7536a30aed27aa7b
Андрій Кулик <andrijkulik@MacBook-Pro-Andrij.local>
2020-10-15 18:01:14 +0300

    waiting for adjust hover styles for component Transaction

--- frontend/src/Components/Transaction/Transaction.js
+++ frontend/src/Components/Transaction/Transaction.js
@@ -13,31 +13,31 @@ const Transaction = () => {
         <span className={styles.transaction__header_item}>Баланс</span>
       </div>
       <ul className={styles.transaction__list}>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Дата</span>
-          <span className={styles.tarnsaction__list_item_value}>04.01.19</span>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Дата</span>
+          <span className={styles.transaction__list_item_value}>04.01.19</span>
         </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Тип</span>
-          <span className={styles.tarnsaction__list_item_value}>-</span>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Тип</span>
+          <span className={styles.transaction__list_item_value}>-</span>
         </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Категория</span>
-          <span className={styles.tarnsaction__list_item_value}>Разное</span>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Категория</span>
+          <span className={styles.transaction__list_item_value}>Разное</span>
         </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Комментарий</span>
-          <span className={styles.tarnsaction__list_item_value}>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Комментарий</span>
+          <span className={styles.transaction__list_item_value}>
             Подарок жене
           </span>
         </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Сумма</span>
-          <span className={styles.tarnsaction__list_item_value}>300.00</span>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Сумма</span>
+          <span className={styles.transaction__list_item_value}>300.00</span>
         </li>
-        <li className={styles.tarnsaction__list_item}>
-          <span className={styles.tarnsaction__list_item_key}>Баланс</span>
-          <span className={styles.tarnsaction__list_item_value}>6 900.00</span>
+        <li className={styles.transaction__list_item}>
+          <span className={styles.transaction__list_item_key}>Баланс</span>
+          <span className={styles.transaction__list_item_value}>6 900.00</span>
         </li>
       </ul>
     </>
