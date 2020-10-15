703c8a5bb23adc4d65c6a2b103e9a0f8a978c9d7
amie-affection <amie.affection@gmail.com>
2020-10-15 14:10:16 +0300

    init navigation

--- frontend/src/Containers/Main/Main.js
+++ frontend/src/Containers/Main/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Navigation from "../../Components/Navigation/Navigation";
 import css from "./Main.module.css";
 
 const Main = () => {
   return (
     <div className={css.main}>
       <Header />
-      {/* Other componnets */}
+      <Navigation />
     </div>
   );
 };
