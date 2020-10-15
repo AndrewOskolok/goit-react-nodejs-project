9c90e3107be92d082daa5b14d8fd39b7bdccf5a8
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-10-15 19:15:14 +0300

    added currency block

--- frontend/src/Containers/Main/Main.js
+++ frontend/src/Containers/Main/Main.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Header from "../../Components/Header/Header";
-import css from "./Main.module.css";
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import CurrencyRate from '../../Components/CurrencyRate/CurrencyRate';
+import css from './Main.module.css';
 
 const Main = () => {
   return (
     <div className={css.main}>
       <Header />
+      <CurrencyRate />
       {/* Other componnets */}
     </div>
   );
