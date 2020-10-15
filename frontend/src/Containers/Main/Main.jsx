dce6c5759c4fb2b2a0407f505e526cccfb57b3e9
Андрій Кулик <andrijkulik@MacBook-Pro-Andrij.local>
2020-10-15 22:50:27 +0300

    before pull of dev

--- frontend/src/Containers/Main/Main.jsx
+++ frontend/src/Containers/Main/Main.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import classNames from "classnames";
 import Header from "../../Components/Header/Header";
-import Tranaction from '../../Components/Transaction/Transaction'
 import css from "./Main.module.css";
 
 const Main = () => {
   return (
-    // <div className={css.main}>
-    //   <Header />
-    //   <div className={css.main__wrapper}>
-    //     <Tranaction/>
-    //   </div>
-    // </div>
-    <div className={css.qw}>
-      <div className={css.wp}>
-<Tranaction/>
+    <div className={css.main}>
+      <Header />
+      <div className={css.main__wrapper}>
+       
       </div>
-    </div>
-    
-    
+    </div>  
   );
 };
 
