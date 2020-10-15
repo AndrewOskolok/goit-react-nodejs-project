5eb75e40098d386a59349e1f15e5f31a38d5e48e
amie-affection <amie.affection@gmail.com>
2020-10-15 21:41:39 +0300

    done main

--- frontend/src/Components/Balance/Balance.jsx
+++ frontend/src/Components/Balance/Balance.jsx
@@ -3,12 +3,10 @@ import css from './Balance.module.css';
 
 const Balance = () => {
   return (
-      <div className={css.balance}>
-          <button className={css.main__balance}>
+          <div className={css.balance}>
               <p className={css.balance__name}>Ваш баланс</p>
               <p className={css.balance__total}><span className={css.balance__marker}>$ </span>24 000.00</p> 
-          </button> 
-</div>
+          </div> 
   );
 };
 
