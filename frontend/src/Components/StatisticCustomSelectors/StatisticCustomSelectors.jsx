c6dabdf4ea7243aecad3ff0c7d8c8bf45af7a3a9
PopeyeIT <igorserov28@gmail.com>
2020-10-16 12:12:43 +0300

    before pull

--- frontend/src/Components/StatisticCustomSelectors/StatisticCustomSelectors.jsx
+++ frontend/src/Components/StatisticCustomSelectors/StatisticCustomSelectors.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import css from './StatisticCustomSelectors.module.css';
@@ -8,10 +8,13 @@ const example = ['Август', 'Сентябрь', 'Октябрь'];
 const StatisticCustomSelectors = ({ startValue }) => {
   const [valueSelector, setValueSelector] = useState(startValue);
   const [isOpen, setIsOpen] = useState(false);
-
+  const ref = useRef();
   const handleChangeSelector = ({ target }) => {
     setIsOpen(state => !state);
     const selectedOption = target.textContent;
+    console.dir(ref.current);
+    ref.current.classList.toggle(`${css.select__head_open}`);
+    // select__head_open
     target.classList.toggle(css.open);
     setValueSelector(selectedOption);
     console.log(target);
@@ -19,7 +22,11 @@ const StatisticCustomSelectors = ({ startValue }) => {
   return (
     <div className={css.select}>
       <input className={css.select__input} type="hidden" />
-      <div onClick={handleChangeSelector} className={css.select__head}>
+      <div
+        onClick={handleChangeSelector}
+        ref={ref}
+        className={css.select__head}
+      >
         {valueSelector}
       </div>
 
