0135c3b72b1dfe0fc56eb025dc23baa9ec9c3a24
amie-affection <amie.affection@gmail.com>
2020-10-19 12:03:07 +0300

    added routing

--- frontend/src/Components/Navigation/Navigation.jsx
+++ frontend/src/Components/Navigation/Navigation.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import classNames from 'classnames';
 import css from './Navigation.module.css';
 
 const Navigation = () => {
     return (
-        <div className={css.main__buttons_wrapper}>
-            <button
-                className={classNames(css.main__btn, css.main__btn_home)}
-            ></button>
-            <button
-                className={classNames(css.main__btn, css.main__btn_statistic)}
-            ></button>
-            <button
-                className={classNames(css.main__btn, css.main__btn_course)}
-            ></button>
-        </div>
+        <BrowserRouter>
+            <nav className={css.main__buttons_wrapper}>
+                <button className={classNames(css.main__btn, css.main__btn_home)}
+                    ><Link to="/"></Link></button>
+                    
+                <button className={classNames(css.main__btn, css.main__btn_statistic)}
+                    ><Link to="/statistic_page"></Link></button>
+                
+                <button className={classNames(css.main__btn, css.main__btn_course)}
+                    ><Link to="/currency_page"></Link></button>
+            </nav>
+            
+            <Switch>
+                <Route exact path="/" />
+                <Route path="/statistic_page" component={} />
+                <Route path="/currency_page" component={}/>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
