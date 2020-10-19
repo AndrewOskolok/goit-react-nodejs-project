39a2ca19f4c44982fadffb8d7afbd7c706045d75
amie-affection <amie.affection@gmail.com>
2020-10-19 11:47:04 +0300

    modified AddTransaction

--- frontend/src/Components/addTransaction/AddTransaction.jsx
+++ frontend/src/Components/addTransaction/AddTransaction.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import css from './AddTransaction.module.css';
 
 const AddTransaction = () => {
+  const [isShowModal, changeIsShowModal] = useState(false);
+
+  const showModal = () => changeIsShowModal(true);
+
   return (
     <div className={css.add__transaction}>
-        <button className={css.add__transaction_btn}>+</button>
+        <button className={css.add__transaction_btn} type="button" onClick={showModal}>+</button>
     </div>
   );
 };
