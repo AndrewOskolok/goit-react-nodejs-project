1044a71855543af1da312f1d35cfdc9b25ba655b
PopeyeIT <igorserov28@gmail.com>
2020-10-19 15:56:05 +0300

    before pull

--- frontend/src/Containers/Statistic/Statistic.js
+++ frontend/src/Containers/Statistic/Statistic.js
@@ -2,15 +2,14 @@ import React from 'react';
 import StatisticCustomSelectors from '../../Components/StatisticCustomSelectors/StatisticCustomSelectors';
 import StatisticList from '../../Components/StatisticList/StatisticList';
 import StatisticChart from '../../Components/StatisticChart/StatisticChart';
-import {
-  filterTypeOperation,
-  findUniqueCategory,
-} from '../../helpers/handleFilterStatistic';
 import css from './Statistic.module.css';
 
 const Statistic = () => {
-  const arrayOfStat = findUniqueCategory();
-  const economic = filterTypeOperation();
+  const arrayOfStat = [];
+  const economic = {
+    income: 22000,
+    expense: 18000,
+  };
   return (
     <section className={css.statistic}>
       <h2 className={css.statistic__title}>Статистика</h2>
@@ -36,7 +35,7 @@ const Statistic = () => {
                   Расходы:
                 </p>
                 <p className={css.statistic__group_result_expense_info}>
-                  {economic.consumption}
+                  {economic.expense}
                 </p>
               </div>
               <div className={css.statistic__group_result_income}>
