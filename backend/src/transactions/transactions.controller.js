18d995d822cab55fb50f0b1e4e789116393be560
PopeyeIT <igorserov28@gmail.com>
2020-10-19 17:32:51 +0300

    added endpoit for static

--- backend/src/transactions/transactions.controller.js
+++ backend/src/transactions/transactions.controller.js
@@ -1,13 +1,16 @@
-const { v4: uuidv4 } = require("uuid");
-const { UserModel } = require("../users/user.model");
-
+const { v4: uuidv4 } = require('uuid');
+const { UserModel } = require('../users/user.model');
+const {
+  findUniqueCategory,
+  countSumByType,
+} = require('../helpers/getUniqueCategory');
 async function createTransaction(req, res) {
   const transactionId = uuidv4();
   const userToUpdate = await UserModel.findByIdAndUpdate(req.user._id, {
     $push: { transactions: { ...req.body, id: transactionId } },
   });
   if (!userToUpdate) {
-    return res.status(404).send({ message: "Unauthorized" });
+    return res.status(404).send({ message: 'Unauthorized' });
   }
   res.status(201).send({ ...req.body, id: transactionId });
 }
@@ -17,7 +20,7 @@ async function deleteTransaction(req, res) {
     $pull: { transactions: { id: `${req.params.transactionId}` } },
   });
   if (!userToUpdate) {
-    return res.status(404).send({ message: "Unauthorized" });
+    return res.status(404).send({ message: 'Unauthorized' });
   }
   return res.status(204).send();
 }
@@ -25,25 +28,41 @@ async function deleteTransaction(req, res) {
 async function updateTransaction(req, res) {
   const userToUpdate = await UserModel.findById(req.user._id);
   if (!userToUpdate) {
-    return res.status(404).send({ message: "Unauthorized" });
+    return res.status(404).send({ message: 'Unauthorized' });
   }
   const transactionToUpdate = userToUpdate.transactions.find(
-    (transaction) => transaction.id === req.params.transactionId
+    transaction => transaction.id === req.params.transactionId,
   );
   const updatedTransaction = { ...transactionToUpdate, ...req.body };
   await UserModel.update(
-    { "transactions.id": req.params.transactionId },
+    { 'transactions.id': req.params.transactionId },
     {
       $set: {
-        "transactions.$": updatedTransaction,
+        'transactions.$': updatedTransaction,
       },
-    }
+    },
   );
   return res.status(204).send(updatedTransaction);
+}
+
+async function filteredStatisticsByDate(req, res) {
+  const { year, month } = req.query;
+  const { user } = req;
+
+  const yearNumber = Number(year);
+  const correctStatsByDate = user.transactions.filter(el => {
+    return el.year === yearNumber && el.month === month;
+  });
+  const sumType = countSumByType(correctStatsByDate);
+  const categories = findUniqueCategory(correctStatsByDate);
+  const result = { sumType, categories, currentBalance: user.currentBalance };
+
+  res.status(200).json(result);
 }
 
 module.exports = {
   createTransaction,
   deleteTransaction,
   updateTransaction,
+  filteredStatisticsByDate,
 };
