c02857627569e081f88b6cf78f02a99ef658a955
Kulyk Andrii <kyluk5@gmail.com>
2020-10-21 14:08:18 +0300

    added changes

--- frontend/src/Components/Transaction/Transaction.jsx
+++ frontend/src/Components/Transaction/Transaction.jsx
@@ -10,7 +10,7 @@ const Transaction = () => {
   const dispatch = useDispatch();
   // const userToken = useSelector((state) => state.auth.token);
   const userToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZjhmMmEwNjZjYmU4NDAwMTcwYzc3M2MiLCJzaWQiOiI1ZjkwMGEzMmQ2NTY4YTAwMTcwMjk2MjUiLCJpYXQiOjE2MDMyNzUzMTQsImV4cCI6MTYwMzI3NzExNH0.K-ekWyj42iExnNoint8-ZdGTumtHOxN1cfFqwFAaIPo";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZjhmMmEwNjZjYmU4NDAwMTcwYzc3M2MiLCJzaWQiOiI1ZjkwMTY1ZGQ2NTY4YTAwMTcwMjk2MjgiLCJpYXQiOjE2MDMyNzg0MjksImV4cCI6MTYwMzI4MDIyOX0.RRDGEnwtRZ11jca6iFvXXZGMt-deWfNpQ5UtBWobzPY";
   const transactions = useSelector((state) => state.transactions);
 
   useEffect(() => {
