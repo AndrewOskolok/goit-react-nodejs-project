b4e5738c768d94c7e218eae26bb8c5a730e58af4
Kulyk Andrii <kyluk5@gmail.com>
2020-10-22 12:17:20 +0300

    before pull reguest

--- frontend/src/redux/opertions/transactionsOperation.js
+++ frontend/src/redux/opertions/transactionsOperation.js
@@ -36,12 +36,19 @@ export const getCurrentTransactions = (token) => async (dispatch) => {
     });
 
     const newData = data.map((item) => {
-      const monthNumber = monthNames.indexOf(item.month) + 1;
+      let monthNumber = monthNames.indexOf(item.month) + 1;
       const newYear = Number(String(item.year).slice(-2));
       if (item.type === "income") {
         item.type = "+";
-      } else if (item.type === "expense") {
+      }
+      if (item.type === "expense") {
         item.type = "-";
+      }
+      if (item.date < 10) {
+        item.date = "0" + String(item.date);
+      }
+      if (monthNumber < 10) {
+        monthNumber = "0" + String(monthNumber);
       }
       return { ...item, month: monthNumber, year: newYear };
     });
