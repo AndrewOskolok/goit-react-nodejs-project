50491ac7aed408193d893e4310ce5c606b4b0ef4
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-10-23 16:37:07 +0300

    created error in redux

--- frontend/src/redux/constants/constants.js
+++ frontend/src/redux/constants/constants.js
@@ -1,1 +1,4 @@
 export const LOADER_TOGGLE = "@loader/toggle";
+export const ERROR_ON = "@error/on";
+export const ERROR_OFF = "@error/off";
+
--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import loader from "./loaderReducer";
+import { error } from "./errorReducer";
 
 export const rootReducer = combineReducers({
   loader,
+  error
 });
