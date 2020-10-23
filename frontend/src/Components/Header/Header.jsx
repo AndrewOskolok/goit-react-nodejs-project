4ddf116943b51086234a30c7a60c25335a5c6c2a
amie-affection <amie.affection@gmail.com>
2020-10-23 17:53:51 +0300

    fixed name-field

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import css from './Header.module.css';
 
 const Header = () => {
+  // const userName = useSelector(state => state.user.userName);
+  
   return (
     <header className={css.header}>
       <div className={css.header__wrapper}>
@@ -9,7 +12,10 @@ const Header = () => {
           <h1 className={css.header__title}>Wallet</h1>
         </a>
         <div className={css.header__login_wrapper}>
-          <p className={css.header__login_name}>Имя</p>
+          <p className={css.header__login_name}>
+            Имя
+            {/* {userName} */}
+          </p>
           <button className={css.header__logout_button}></button>
         </div>
       </div>
