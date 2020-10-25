392ef778d866b200eab5c11a3803a894cc101b43
Kulyk Andrii <kyluk5@gmail.com>
2020-10-25 18:43:54 +0200

    added logic when delete transaction

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -1,9 +1,18 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { getUser } from "../actions/userAction";
-import balanceAction from "../actions/categoriesActions.js"
+import balanceAction from "../actions/categoriesActions.js";
+import { editCurrentBalance } from "../actions/transactionActions";
 
 const initialState = null;
 
 export default createReducer(initialState, {
-  [getUser]: (_, {payload}) => payload,
-  [balanceAction.editCurrentBalance]: (state, {payload}) => ({...state, currentBalance: payload})});
+  [getUser]: (_, { payload }) => payload,
+  [balanceAction.editCurrentBalance]: (state, { payload }) => ({
+    ...state,
+    currentBalance: payload,
+  }),
+  [editCurrentBalance]: (state, { payload }) => ({
+    ...state,
+    currentBalance: payload,
+  }),
+});
